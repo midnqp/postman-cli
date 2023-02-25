1b5f6d0b2d577fba992ad9bc2fca17ea8b471f3b
midnqp <midnightquantumprogrammer@gmail.com>
2023-02-25 12:43:08 +0600

    chore: new lifecycle commands
    chore: add bundle deps

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,10 +1,11 @@
 const {resolve} = require('path')
 module.exports = {
-  mode: "production",
-  entry: "./dist/src/index.js",
-  target: "node",
-  output: {
-    path: resolve(__dirname, "dist", "webpack"),
-    chunkFormat: "commonjs",
-  },
+	mode: "production",
+	entry: "./dist/src/index.js",
+	target: "node",
+	output: {
+		path: resolve(__dirname, "dist"),
+		filename: 'bundle.cjs',
+		chunkFormat: "commonjs",
+	},
 };
